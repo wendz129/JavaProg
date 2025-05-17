5f0e227573c083110024a2b90a05f41851ac2887
We N D eLL <159757637+wendz129@users.noreply.github.com>
2025-05-17 14:27:43 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,7 +1,12 @@
-class Main {
-    
-    public static void main(String[] args){
-        System.out.println("Hello World");
-    }
+// sample file
 
-}
+public class Main{
+    public static void main(String[] args) {
+        //create and instance of a Toy class = Toy object
+
+        /* this
+         * is a
+         *  multiline
+         * 
+         */
+    }
