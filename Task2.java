4629346afa0cfd2eebc6b09ee1e1ad7cf454aa2f
We N D eLL <159757637+wendz129@users.noreply.github.com>
2025-05-17 14:22:39 +0800

    Add files via upload

---
+++ Task2.java
@@ -0,0 +1,13 @@
+public class Main {
+	public static void main(String[] args) {
+		byte b = 72; 
+		short s = 3110;
+		int i = 3100;
+		char c ='';
+		boolean bool = true;
+		float f = 2.0f;
+
+		String output = "" + b + s + c + i + c + f +" + bool;
+		System.out.println(output);
+	}
+}
